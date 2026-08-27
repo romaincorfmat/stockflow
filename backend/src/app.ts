96d308e7ebb6dbd6d32e65df459f8b515e34ba44
romaincorfmat <romain.corfmat@gmail.com>
2026-08-27 18:19:15 +0200

    chore: Set up TypeScript backend

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app =express();
+
+app.use(express.json());
+
+app.get('/api/health', (_req, res) => {
+    res.json({status: 'ok'});
+});
+
+app.listen(3000, () => {
+    console.log('API running on http://localhost:3000')
+})
