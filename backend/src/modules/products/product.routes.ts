780b2349e14021c6ea4a792a595c318d7818181c
romaincorfmat <romain.corfmat@gmail.com>
2026-08-29 09:18:18 +0200

    feat: Add Delete product endpoint (#10)

--- backend/src/modules/products/product.routes.ts
+++ backend/src/modules/products/product.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import {
   createProductController,
+  deleteProductController,
   getProductByIdController,
   getProductsController,
 } from "./product.controller.js";
@@ -11,3 +12,4 @@ export const productRouter = Router();
 productRouter.get("/", getProductsController);
 productRouter.get("/:id", getProductByIdController);
 productRouter.post("/", createProductController);
+productRouter.delete("/:id", deleteProductController);
